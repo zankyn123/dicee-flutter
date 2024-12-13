a252eaeefeb2403c6858b8c7bf2204f9da2cb174
Manh Hung <>
2024-12-13 17:50:55 +0700

    Finish app

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/hung/Documents/Flutter-SDK/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/hung/Documents/Learn flutter/5.dicee_flutter"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/hung/Documents/Learn flutter/5.dicee_flutter/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/hung/Documents/Learn flutter/5.dicee_flutter/.dart_tool/package_config.json"
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,98 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
-    MaterialApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          elevation: 2,
+          shadowColor: Colors.red.shade900,
+          title: Center(
+              child: Text(
+            'Dicee',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500),
+          )),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
       ),
-    ),
-  );
-}
-
-class DicePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+    );
   }
 }
+
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int indexLeft = 1;
+  int indexRight = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: TextButton(
+              style: ButtonStyle(
+                overlayColor: WidgetStateProperty.all(Colors.transparent),
+                padding: WidgetStateProperty.all(EdgeInsets.all(16)),
+              ),
+              onPressed: () {
+                changeDiceFace(DiceIndex.first);
+              },
+              child: Image.asset('images/dice$indexLeft.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              style: ButtonStyle(
+                overlayColor: WidgetStateProperty.all(Colors.transparent),
+                padding: WidgetStateProperty.all(EdgeInsets.all(16)),
+              ),
+              onPressed: () {
+                changeDiceFace(DiceIndex.second);
+              },
+              child: Image.asset('images/dice$indexRight.png'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void changeDiceFace(DiceIndex index) {
+    // >= 1 & < 6
+    setState(() {
+      // switch (index) {
+      //   case DiceIndex.first:
+      //     indexLeft = Random().nextInt(6) + 1;
+      //   case DiceIndex.second:
+      //     indexRight = Random().nextInt(6) + 1;
+      // }
+      indexLeft = Random().nextInt(6) + 1;
+      indexRight = Random().nextInt(6) + 1;
+    });
+  }
+}
+
+enum DiceIndex { first, second }
